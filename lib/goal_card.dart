991c4884bd7b38eb56388f19a30b754ba3033bbd
ihmaisrael@gmail.com <ihmaisrael@gmail.com>
2020-09-03 15:41:20 -0500

    TextFormField - Se agrego los campos de texto sobre los cuales trabajaremos para generar la siguiente pantalla

--- lib/goal_card.dart
+++ lib/goal_card.dart
@@ -11,14 +11,72 @@ class _GoalCardState extends State<GoalCard> {
   double anchoPantalla = MediaQuery.of(context).size.width;
     return Center(
      child: Container(
-       margin: EdgeInsets.all(15),
-       height: 100,
+       margin: EdgeInsets.all(10),
        width: anchoPantalla * 0.9,
        decoration: BoxDecoration(
          color: Color.fromRGBO(223, 255, 255, 1),
          borderRadius: BorderRadius.circular(15.0),
        ),
+       child: Row(
+         crossAxisAlignment: CrossAxisAlignment.start,
+         children: [
+           Container(
+             padding: EdgeInsets.only(left: 10, bottom: 13),
+             child:  _actividadInput(),
+             width: anchoPantalla*0.45,
+           ),
+           Spacer(),
+           Container(
+             padding: EdgeInsets.only(left: 10, bottom: 13),
+             child:  _inputConcentracion(),
+             width: anchoPantalla*0.15,
+           ),
+           Spacer(),
+           Container(
+             padding: EdgeInsets.only(left: 10, bottom: 13),
+             child:  _inputDescanso(),
+             width: anchoPantalla*0.15,
+           ),
+         ],
+       ),
      ),
    );
+  }//build
+  Widget _actividadInput(){
+    return TextFormField (
+      decoration: InputDecoration(
+        labelText: "Actividad",
+        labelStyle: TextStyle( fontSize: 13),
+      ),
+      onChanged: (activity){
+        print(activity);
+      },
+    );
+  }// _actividad Input
+  
+  Widget _inputConcentracion(){
+    return TextFormField (
+      keyboardType: TextInputType.numberWithOptions(decimal: true),
+      decoration: InputDecoration(
+        labelText: "Focus",
+        labelStyle: TextStyle( fontSize: 13),
+      ),
+      onChanged: (tiempoConcentracion){
+        print(tiempoConcentracion);
+      },
+    );
+  }
+
+  Widget _inputDescanso(){
+    return TextFormField (
+      keyboardType: TextInputType.numberWithOptions(decimal: true),
+      decoration: InputDecoration(
+        labelText: "Relax",
+        labelStyle: TextStyle( fontSize: 13),
+      ),
+      onChanged: (tiempoDescanso){
+        print(tiempoDescanso);
+      },
+    );
   }
 }
