6500ff495f6b38b0ae40aaf7981dbb0c0582bd5c
Itzel Gomez <itzelgomez@MacBook-Pro-de-Itzel.local>
2020-09-05 15:03:26 -0500

    AquadoroStack - Se agrega una imagen de fondo y los textos del tipo de actividad y tiempo

--- lib/aquadoro.dart
+++ lib/aquadoro.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Aquadoro extends StatefulWidget {
-
   Aquadoro({
     this.actividad,
     this.tConcentracion,
@@ -17,35 +16,66 @@ class Aquadoro extends StatefulWidget {
 }
 
 class _AquadoroState extends State<Aquadoro> {
+  String tipoActividad = 'Focus';
+  String tiempoPantalla = '12:00';
+
   @override
   Widget build(BuildContext context) {
+    double ancho = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text('Aquadoro'),
-        backgroundColor:  Colors.cyan[600],
+        backgroundColor: Colors.cyan[600],
       ),
       body: Stack(
         children: [
           Container(color: Colors.cyan[600]),
-          Column(
-
-            children: [
-              Center(child: Text('Hola xD')),
-              Center(child: Text('Actividad: ${widget.actividad}', style: TextStyle(
-                fontSize: 25, color: Colors.blueGrey[50]
-              ),)),
-              Center(child: Text('tconcentracion :${widget.tConcentracion}', style: TextStyle(
-                fontSize: 25, color: Colors.blueGrey[50]
-              ),)),
-              Center(child: Text('tDescanso : ${widget.tDescanso}', style: TextStyle(
-                fontSize: 25, color: Colors.blueGrey[50]
-              ),)),
-
-            ],
-          ),
+          _aquadoroStack(ancho),
         ],
       ),
+    );
+  }
+
+  Widget _aquadoroStack(double ancho) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          width: 357,
+          height: 357,
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            image: DecorationImage(
+                image: AssetImage("assets/Acuadoro.png"), fit: BoxFit.cover),
+          ),
+        ),
+        Positioned(
+          top: 160,
+          left: ancho * 0.25,
+          child: Container(
+            height: 100,
+            width: 170,
+            child: Column(
+              children: <Widget>[
+                Center(
+                  child: Text(
+                    tipoActividad,
+                    style: TextStyle(fontSize: 25, color: Colors.blueGrey[50]),
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    tiempoPantalla,
+                    style: TextStyle(fontSize: 25, color: Colors.blueGrey[50]),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
